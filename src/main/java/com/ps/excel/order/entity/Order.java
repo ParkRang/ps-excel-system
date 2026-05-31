4827cae8f7c423d183be857632833d340d2fc0ed
parkrang <namid0711@naver.com>
2026-06-01 02:24:27 +0900

    feat Order.java

---
+++ src/main/java/com/ps/excel/order/entity/Order.java
@@ -0,0 +1,27 @@
+package com.ps.excel.order.entity;
+
+import jakarta.persistence.*;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "order")
+public class Order {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    private String user_name;
+
+    private String product_name;
+
+    private String category;
+
+    private Integer amount;
+
+    private String status;
+
+    private LocalDateTime order_date;
+
+}
