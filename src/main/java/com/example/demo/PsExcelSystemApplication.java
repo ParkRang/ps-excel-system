f61ef514ce99f75b970839847cf7afafa1b1d033
parkrang <namid0711@naver.com>
2026-05-31 18:10:08 +0900

    add first setup

---
+++ src/main/java/com/example/demo/PsExcelSystemApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PsExcelSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PsExcelSystemApplication.class, args);
+	}
+
+}
