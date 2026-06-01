ae9cfb78cb8a932894fae62816596f472a800934
parkrang <namid0711@naver.com>
2026-06-02 07:04:07 +0900

    fix PsExcelSystemApplication

--- src/main/java/com/ps/excel/PsExcelSystemApplication.java
+++ src/main/java/com/ps/excel/PsExcelSystemApplication.java
@@ -2,8 +2,10 @@ package com.ps.excel;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableAsync;
 
 @SpringBootApplication
+@EnableAsync
 public class PsExcelSystemApplication {
 
 	public static void main(String[] args) {
