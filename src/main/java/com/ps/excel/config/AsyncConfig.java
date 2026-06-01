37ebbd578845332ac9701bc2541441e07222de07
parkrang <namid0711@naver.com>
2026-06-02 07:04:18 +0900

    feat AsyncConfig

---
+++ src/main/java/com/ps/excel/config/AsyncConfig.java
@@ -0,0 +1,29 @@
+package com.ps.excel.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.annotation.EnableAsync;
+import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
+
+import java.util.concurrent.Executor;
+
+@Configuration
+@EnableAsync
+public class AsyncConfig {
+
+    @Bean(name = "excelExecutor")
+    public Executor excelExecutor() {
+
+        ThreadPoolTaskExecutor executor = new ThreadPoolTaskExecutor();
+
+        executor.setCorePoolSize(2);
+        executor.setMaxPoolSize(4);
+        executor.setQueueCapacity(10);
+
+        executor.setThreadNamePrefix("excel-");
+
+        executor.initialize();
+
+        return executor;
+    }
+}
