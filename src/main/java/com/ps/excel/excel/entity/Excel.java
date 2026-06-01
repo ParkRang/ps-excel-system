0491a14338f6f4b3624c3c07edc87ab1e34ac68e
parkrang <namid0711@naver.com>
2026-06-01 20:33:49 +0900

    fix excel.java

--- src/main/java/com/ps/excel/excel/entity/Excel.java
+++ src/main/java/com/ps/excel/excel/entity/Excel.java
@@ -10,21 +10,43 @@ import java.time.LocalDateTime;
 @Table(name = "excel")
 @Builder(toBuilder = true)
 @Getter
+@AllArgsConstructor
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class Excel {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
 
-    private LocalDateTime requested_at;
+    @Column(name = "requested_at")
+    private LocalDateTime requestedAt;
 
-    private LocalDateTime started_at;
+    @Column(name = "started_at")
+    private LocalDateTime startedAt;
 
-    private LocalDateTime finished_at;
+    @Column(name = "finished_at")
+    private LocalDateTime finishedAt;
 
     @Enumerated(EnumType.STRING)
     private JobStatus status;
 
     private String filepath;
+
+    // status 전환
+
+    public void start(){
+        this.status = JobStatus.PROCESSING;
+        this.startedAt = LocalDateTime.now();
+    }
+
+    public void finish(String filepath){
+        this.status = JobStatus.DONE;
+        this.finishedAt = LocalDateTime.now();
+        this.filepath = filepath;
+    }
+
+    public void fail(){
+        this.status = JobStatus.FAILED;
+    }
 
 }
